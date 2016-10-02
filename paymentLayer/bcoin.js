b61a3f06301e53f4bdc8d9994b8512a90c5a91b8
Adam Soltys <asoltys@gmail.com>
2016-10-01 21:55:46 -0700

    include bcoin locally

--- paymentLayer/bcoin.js
+++ paymentLayer/bcoin.js
@@ -2,35 +2,60 @@ const log = require('debug')('VM:PaymentLayer:bcoin')
 
 const Kefir = require('kefir')
 const Promise = require('bluebird')
-const bcoin = require('bcoin');
+const bcoin = require('bcoin').set('testnet');
 
 const config = require('../config')
 const addresses = config.map(product => product.address);
 
-var chain = new bcoin.chain({
-  db: 'leveldb',
-  // A custom chaindb location:
-  location: process.env.HOME + '/chain.db',
-  spv: true
-});
+function configure(products) {
+  var chain = new bcoin.chain({
+    db: 'leveldb',
+    // A custom chaindb location:
+    location: process.env.HOME + '/chain.db',
+    spv: true
+  });
 
-var pool = new bcoin.pool({
-  chain: chain,
-  spv: true,
-  size: 1,
-  maxPeers: 1
-});
+  var pool = new bcoin.pool({
+    chain: chain,
+    spv: true,
+    size: 1,
+    maxPeers: 1
+  });
 
-pool.open(err => {
-    addresses.forEach((a) => pool.watchAddress(a));
+  return Promise.fromCallback(done => {
+    pool.open(err => {
+      addresses.forEach((a) => pool.watchAddress(a));
 
-    pool.connect();
-    pool.startSync();
+      pool.connect();
+      pool.startSync();
 
-    pool.on('error', err => { /* keep calm and bitcoin on */ });
-    done(null, pool); //addressWatcher
-})
+      pool.on('error', err => { console.log(err) });
+      done(null, pool);
+    });
+  });
+}
 
+function parse(tx){
+  payment = {
+    txid: tx.hash,
+    received: tx.value,
+    address: tx.outputs.filter(output => addresses.include(output.address.toBase58()))[0]
+  }
+  console.log(payment);
+  return payment;
+}
+
+module.exports = configure(products)
+  .then(watcher => {
+    console.log('Starting up bcoin')
+    return Kefir.fromEvents(watcher, 'tx')
+      .log('bcoin tx seen')
+      .filter(tx => tx.confirmations < 2)
+      .map(parse)
+    })
+
+
+/*
 let bcoinTxStream = Kefir
     .fromEvents(pool, 'tx')
     .map(tx => {
@@ -39,6 +64,7 @@ let bcoinTxStream = Kefir
         received: tx.value,
         address: tx.outputs.filter(output => addresses.include(output.address.toBase58()))[0]
       }
-    })
+    }).log()
 
 module.exports = Promise.resolve(bcoinTxStream)
+*/
